61581da7e632dc836c68b902c5aa9b6196ad2442
Jeong-su Shin <jeshin@c6r3s1.42seoul.kr>
2024-10-25 15:36:25 +0900

    Add ex00

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,9 +1,11 @@
 #include <string>
 #include <iostream>
 #include "ScalarConverter.hpp"
+#include <iomanip>
+
+typedef long long ll;
 
 //static members
-static std::string sTypes[]={"Char","Int","Float","Double"};
 static std::string sPseudoLiteralsOfFloat[]={"+inff","-inff","nanf"};
 static std::string sPseudoLiteralsOfDouble[]={"+inf","-inf","nan"};
 static std::string sType;
@@ -31,7 +33,10 @@ static void	checkInput(std::string input){
 	}
 	//check input is char
 	if (input.size()==1){
-		sType = "char";
+		sType="char";
+		if (isdigit(input[0])){
+			sType = "int";
+		}
 		return ;
 	}
 	//check input is pseudo literals of float
@@ -54,11 +59,12 @@ static void	checkInput(std::string input){
 	int dotCnt=0;
 	int fCnt=0;
 	int	signCnt=0;
-	size_t i=-1;
+	size_t	i=-1;
 	size_t	size=input.size();
 
-	if (input[0]=='-' || input [0]=='+')
+	if (input[0]=='-' || input [0]=='+'){
 		i++;signCnt++;
+	}
 	while (++i < size){
 		if (isdigit(input[i]))
 			continue;
@@ -66,17 +72,21 @@ static void	checkInput(std::string input){
 			case('.'):
 				dotCnt++;
 				if (i == 0 || (signCnt && i == 1) || (i == size-1) || (dotCnt > 1))
-					throw (ScalarConverter::InvaildInputException("\e[0;31mError: Invaild input\e[0;m\n"));
+					throw (ScalarConverter::InvaildInputException("\e[0;31mError: Invaild input, dot\e[0;m\n"));
+				break;
 			case('f'):
-				if (i != size-1)
-					throw (ScalarConverter::InvaildInputException("\e[0;31mError: Invaild input\e[0;m\n"));
 				fCnt++;
+				if (i != size-1)
+					throw (ScalarConverter::InvaildInputException("\e[0;31mError: Invaild input, f\e[0;m\n"));
+				break;
 			default:
-				throw (ScalarConverter::InvaildInputException("\e[0;31mError: Invaild input\e[0;m\n"));
+				throw (ScalarConverter::InvaildInputException("\e[0;31mError: Invaild input, text\e[0;m\n"));
 		}
 	}
 	if (fCnt && !dotCnt)
-		throw (ScalarConverter::InvaildInputException("\e[0;31mError: Invaild input\e[0;m\n"));
+		throw (ScalarConverter::InvaildInputException("\e[0;31mError: Invaild input, no dot float\e[0;m\n"));
+	if (!dotCnt && input[0] == '0')
+		throw (ScalarConverter::InvaildInputException("\e[0;31mError: Invaild input, not decimal\e[0;m\n"));
 	if (fCnt)
 		sType="float";
 	else if (dotCnt)
@@ -84,13 +94,151 @@ static void	checkInput(std::string input){
 	else
 		sType="int";
 }
+void	ft_stoc(std::string& input){
+	//char
+	if (isprint(input[0]))
+		std::cout<<"char: "<<static_cast<char>(input[0])<<"\n";
+	else
+		std::cout<<"char: Non displayable\n";
+	//int
+	std::cout<<"int: "<<static_cast<int>(input[0])<<"\n";
+	//float
+	std::cout<<std::fixed<<std::setprecision(1);
+	std::cout<<"float: "<<static_cast<float>(input[0])<<"f"<<"\n";
+	//double
+	std::cout<<"double: "<<static_cast<double>(input[0])<<"\n";
+}
+void	ft_stoi(std::string& input){
+	ll tmp;
+	//convert string to long long type
+	try{
+		tmp = std::stoll(input);
+	}catch(std::exception& e){
+		std::cerr<<"\e[0;31mError: "<<e.what()<<"\e[0;m\n";
+		return ;
+	}
+	//char
+	if (tmp != static_cast<char>(tmp))
+		std::cout<<"char: Overflow\n";
+	else if (!isprint(tmp))
+		std::cout<<"char: Non displayable\n";
+	else
+		std::cout<<"char: "<<static_cast<char>(tmp)<<"\n";
+	//int
+	if (tmp != static_cast<int>(tmp))
+		std::cout<<"int: Overflow\n";
+	else
+		std::cout<<"int: "<<static_cast<int>(tmp)<<"\n";
+	//float
+	std::cout<<std::fixed<<std::setprecision(1);
+	std::cout<<"float: "<<static_cast<float>(tmp)<<"f"<<"\n";
+	//double
+	std::cout<<"double: "<<static_cast<double>(tmp)<<"\n";
+}
+void	ft_stof(std::string input){
+	double tmp;
+	//convert string to long long type
+	try{
+		tmp = std::stod(input);
+	}catch(std::exception& e){
+		std::cerr<<"\e[0;31mError: "<<e.what()<<"\e[0;m\n";
+		return ;
+	}
+
+	//char
+	if (static_cast<int>(tmp) != static_cast<char>(tmp)){
+		std::cout<<"char: Overflow\n";
+	}else if (!isprint(tmp)){
+		std::cout<<"char: Non displayable\n";
+	}else{
+		std::cout<<"char: "<<static_cast<char>(tmp)<<"\n";
+	}
+	//int
+	if (static_cast<long long>(tmp) != static_cast<int>(tmp)){
+		std::cout<<"int: Overflow\n";
+	}else{
+		std::cout<<"int: "<<static_cast<int>(tmp)<<"\n";
+	}
+	std::cout<<std::fixed<<std::setprecision(1);
+	std::cout<<"float: "<<static_cast<float>(tmp)<<"f"<<"\n";
+	std::cout<<"double: "<<static_cast<double>(tmp)<<"\n";
+}
+void	ft_stod(std::string input){
+	double tmp;
+
+	//convert string to long long type
+	try{
+		tmp = std::stod(input);
+	}catch(std::exception& e){
+		std::cerr<<"\e[0;31mError: "<<e.what()<<"\e[0;m\n";
+		return ;
+	}
+
+	//char
+	if (static_cast<int>(tmp) != static_cast<char>(tmp))
+		std::cout<<"char: Overflow\n";
+	else if (!isprint(static_cast<char>(tmp)))
+		std::cout<<"char: Non displayable\n";
+	else
+		std::cout<<"char: "<<static_cast<char>(tmp)<<"\n";
+	//int
+	if (static_cast<long long>(tmp) != static_cast<int>(tmp))
+		std::cout<<"int: Overflow\n";
+	else
+		std::cout<<"int: "<<static_cast<int>(tmp)<<"\n";
+	//float
+	std::cout<<std::fixed<<std::setprecision(1);
+	std::cout<<"float: "<<static_cast<float>(tmp)<<"f"<<"\n";
+	//double
+	std::cout<<"double: "<<static_cast<double>(tmp)<<"\n";
+}
 
 void	ScalarConverter::converter(std::string input){
+	// input check
 	try{
 		checkInput(input);
 	}catch(std::exception& e){
-		std::cerr<<e.what(); return ;
+		std::cerr<<e.what();
+		return ;
 	}
-	std::cout<<sType<<"\n";
-	std::cout<<sPseudoLiteral<<"\n";
+	//handle pseudo literal
+	if (sPseudoLiteral.size()){
+		std::cout<<"char: impossible\n";
+		std::cout<<"int: impossible\n";
+		
+		if (sType == "float"){ //in case of float pseudo literal
+			std::cout<<"float: "<<sPseudoLiteral<<"\n";
+			sPseudoLiteral.pop_back();
+			std::cout<<"double: "<<sPseudoLiteral<<"\n";
+		}
+		else{ //in case of double pseudo literal
+			std::cout<<"float: "<<sPseudoLiteral+"f"<<"\n";
+			std::cout<<"double: "<<sPseudoLiteral<<"\n";
+		}
+		return ;
+	}
+	// get type in type table
+	std::string typeTable[]={"char","int","float","double"};
+	int pos;
+	for(pos=0;pos<4;pos++){
+		if (sType == typeTable[pos]) break;
+	}
+	enum eTypeTable{
+		eChar=0,eInt=1,eFloat=2,eDouble=3
+	};
+	// convert input to suitable type
+	switch(pos){
+		case(eChar):
+			ft_stoc(input);
+			break;
+		case(eInt):
+			ft_stoi(input);
+			break;
+		case(eFloat):
+			ft_stof(input);
+			break;
+		case(eDouble):
+			ft_stod(input);
+			break;
+	}
 }
